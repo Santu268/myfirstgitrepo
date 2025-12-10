7e5bea889d3607ef561430ed21f40a0a10ee0a8e
Santu268 <112721198+Santu268@users.noreply.github.com>
2025-12-11 00:22:44 +0530

    Rename EC2 instance tag from 'Solace-server1' to 'Solace-server2'
    adding another instance

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "two" {
   ami             = "ami-08d7aabbb50c2c24e"
   instance_type   = "t3.micro"
   tags={
-    Name = "Solace-server1"
+    name = "Solace-server2"
   }
 }
